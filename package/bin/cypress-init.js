14b3a38359f1df4703069921535f0083205eae76
Youssef <sharief@aucegypt.edu>
2020-08-29 17:16:50 -0700

    Update package

--- package/bin/cypress-init.js
+++ package/bin/cypress-init.js
@@ -1,8 +1,15 @@
 #!/usr/bin/env node
+
 const fs = require('fs')
+const { resolve } = require('path')
 const addHygenFiles = require('../lib/scripts/add-hygen-files');
 
 fs.mkdirSync('./cypress/fixtures', { recursive: true });
 fs.mkdirSync('./cypress/integration', { recursive: true });
+
+fs.copyFile(resolve(__dirname, `../templates/cypress.json`), './', (err) => {
+    if (err) throw err;
+    console.log('added cypress.json file')
+});
 
 addHygenFiles('cypress')
